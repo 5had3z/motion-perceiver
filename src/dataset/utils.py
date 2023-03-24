converted data vis to new framework, bugfix roadgraph offset calculation for sdc frame, updated train script to reflect changes in konductor api
Signed-off-by: Bryce Ferenczi <bryce.ferenczi@monash.edu>

src/dataset/utils.py
@@ -1,114 +1,14 @@
-from pathlib import Path
-from typing import Any, Dict
-import os
+"""
+Extra utilities for calculating the maximum map size and 
+the percentage of vehicles in the scene occluded
+"""
+from typing import Dict
 
-from easydict import EasyDict
-from nvidia.dali.plugin.pytorch import DALIGenericIterator
 import torch
+from nvidia.dali.plugin.pytorch import DALIGenericIterator
 
 
-from nnet_training.utilities import comm
-
-try:
-    from .waymo import waymo_motion_pipe
-    from .interaction import interation_pipeline
-    from . import visualisation as mv
-except ImportError:
-    from waymo import waymo_motion_pipe
-    from interaction import interation_pipeline
-    import visualisation as mv
-
-
-def get_dali_dataloader(
-    dataset_config: EasyDict, num_threads: int, val_only: bool = False
-) -> Dict[str, DALIGenericIterator]:
-    """Return DALIGenericIterator for as Dataloader, mostly used for tf.Record datasets"""
-    pipe_kwargs = {
-        "shard_id": comm.get_rank(),
-        "num_shards": comm.get_world_size(),
-        "num_threads": num_threads,
-        "device_id": torch.cuda.current_device(),
-        "batch_size": dataset_config.batch_size // comm.get_world_size(),
-    }
-
-    dataset_name = dataset_config.type.lower()
-    if dataset_name == "waymo_motion":
-        waymo_root = Path(os.environ.get("DATA_PATH", "/data"))
-        pipes = [
-            waymo_motion_pipe(
-                record_root=waymo_root / "validation",
-                random_shuffle=False,
-                **pipe_kwargs,
-                **dataset_config.args,
-            )
-        ]
-        reader_names = ["waymo_validation"]
-        if not val_only:
-            pipes.append(
-                waymo_motion_pipe(
-                    record_root=waymo_root / "training",
-                    random_shuffle=dataset_config.shuffle,
-                    **pipe_kwargs,
-                    **dataset_config.args,
-                )
-            )
-            reader_names.append("waymo_training")
-
-        output_map = ["agents", "agents_valid"]
-        if dataset_config.args.get("road_features", False):
-            output_map.extend(["roadgraph", "roadgraph_valid"])
-        if dataset_config.args.get("roadmap", False):
-            output_map.append("roadmap")
-        if dataset_config.args.get("signal_features", False):
-            output_map.extend(["signals", "signals_valid"])
-        if dataset_config.args.get("occupancy_size", 0) > 0:
-            output_map.extend(["heatmap", "time_idx"])
-        if dataset_config.args.get("scenario_id", False):
-            output_map.append("scenario_id")
-
-    elif dataset_name == "interaction":
-        interaction_root = (
-            Path(os.environ.get("INTERACTION_PATH", "/data")) / "tfrecord"
-        )
-        pipes = [
-            interation_pipeline(
-                record_file=interaction_root / "interaction_val.tfrecord",
-                random_shuffle=False,
-                **pipe_kwargs,
-                **dataset_config.args,
-            )
-        ]
-        reader_names = ["interaction_val"]
-        if not val_only:
-            pipes.append(
-                interation_pipeline(
-                    record_file=interaction_root / "interaction_train.tfrecord",
-                    random_shuffle=dataset_config.shuffle,
-                    **pipe_kwargs,
-                    **dataset_config.args,
-                )
-            )
-            reader_names.append("interaction_train")
-
-        output_map = ["agents", "agents_valid"]
-        if dataset_config.args.get("roadmap", False):
-            output_map.append("roadmap")
-        if dataset_config.args.get("occupancy_size", 0) > 0:
-            output_map.extend(["heatmap", "time_idx"])
-    else:
-        raise NotImplementedError(dataset_name)
-
-    dataloaders = {}
-    for pipe, rname, name in zip(pipes, reader_names, ["Validation", "Training"]):
-        pipe.build()
-        dataloaders[name] = DALIGenericIterator(
-            pipe, output_map, reader_name=rname, auto_reset=True
-        )
-
-    return dataloaders
-
-
-def check_maximum_map_size(dataloaders) -> None:
+def check_maximum_map_size(dataloaders: Dict[str, DALIGenericIterator]) -> None:
     """
     Determine the maximum map size for waymo motion.
     This is required for calibration of the output map.
@@ -148,8 +48,8 @@ def check_maximum_map_size(dataloaders) -> None:
                 y_min = filt[..., 1].min(dim=-1)[0]
 
                 # Find range
-                max_x_range = max((x_max - x_min).max(), max_x_range)
-                max_y_range = max((y_max - y_min).max(), max_y_range)
+                max_x_range = torch.max((x_max - x_min).max(), max_x_range)
+                max_y_range = torch.max((y_max - y_min).max(), max_y_range)
                 pbar.update(1)
                 pbar.set_description(
                     f"max map size {max_x_range:.1f}x{max_y_range:.1f}", refresh=True
@@ -157,7 +57,7 @@ def check_maximum_map_size(dataloaders) -> None:
 
 
 def evaluate_vehicle_occulsions(
-    dataloaders: Dict[str, Any], max_samples: int = 0
+    dataloaders: Dict[str, DALIGenericIterator], max_samples: int = 0
 ) -> None:
     """Determine the percentage of observed vehciles remaining at later timesteps"""
     from tqdm.auto import tqdm
@@ -211,52 +111,3 @@ def evaluate_vehicle_occulsions(
     plt.ylabel("% Agents Observed")
     plt.tight_layout()
     plt.savefig("waymo_agent_decay.png")
-
-
-def test_dali_loader() -> None:
-    """Yield some data"""
-
-    loader_config = {
-        "type": "waymo_motion",
-        "batch_size": 4,
-        "shuffle": True,
-        "drop_last": True,
-        "n_workers": 8,
-        "args": {
-            "map_normalize": 40.0,
-            "full_sequence": True,
-            "occupancy_size": 256,
-            "heatmap_time": list(range(0, 91, 10)),
-            "filter_future": True,
-            # "separate_classes": True,
-            # "random_heatmap_count": 4,
-            # "random_heatmap_minmax": [1, 40],
-            "signal_features": True,
-            "roadmap": True,
-            "use_sdc_frame": True,
-            "waymo_eval_frame": True,
-        },
-    }
-
-    loaders = get_dali_dataloader(loader_config, num_threads=loader_config["n_workers"])
-
-    # evaluate_vehicle_occulsions(loaders, 0)
-    # return
-
-    # check_maximum_map_size(loaders)
-    # return
-
-    for data in loaders["Validation"]:
-        data: Dict[str, torch.Tensor] = data[0]  # remove list dim
-        # visualise_roadgraph(data["roadgraph"], data["roadgraph_valid"])
-        # mv.visualise_roadmap(data["roadmap"])
-        # mv.overlay_roadmap_and_occupancy(
-        #     data["roadmap"], data["heatmap"], data["signals"]
-        # )
-        mv.visualize_sequence(data)
-        # visualise_occupancy_map(data)
-        break
-
-
-if __name__ == "__main__":
-    test_dali_loader()

src/dataset/visualisation.py
@@ -8,7 +8,7 @@ from torch import Tensor
 from matplotlib import pyplot as plt
 
 
-def visualize_sequence(data: Dict[str, Tensor]) -> None:
+def sequence(data: Dict[str, Tensor]) -> None:
     """Plot world-lines of instances and heatmap at chosen keyframe"""
 
     for i, (heatmap, bt_idx) in enumerate(zip(data["heatmap"], data["time_idx"])):
@@ -67,7 +67,7 @@ def pose_to_poly(pose_data: np.ndarray) -> np.ndarray:
     return rot_mat @ bbox + pose_data[1:3, None]
 
 
-def occupancy_image(
+def render_occupancy(
     sample_data: Tensor, scale: float = 1.0, padding: float = 40.0
 ) -> np.ndarray:
     """
@@ -101,14 +101,12 @@ def occupancy_image(
     return occupancy_map
 
 
-def visualise_occupancy_map(data: Dict[str, Tensor]) -> None:
-    """"""
-
+def occupancy_from_current_pose(data: Dict[str, Tensor]) -> None:
+    """Requires loading of raw waymo tf data i.e. raw "current" frame"""
     for i, state_ in enumerate(data["current"]):
         valid_mask = state_[..., 0] != 0
         valid_data = state_[valid_mask]
-        occ = occupancy_image(valid_data)
-
+        occ = render_occupancy(valid_data)
         plt.figure(f"occupancy_{i}", figsize=(20, 20))
         # invert y so min-y is bottom of the image to match scatter plot
         occ = cv2.flip(occ, 0)
@@ -117,7 +115,7 @@ def visualise_occupancy_map(data: Dict[str, Tensor]) -> None:
         plt.savefig(f"occupancy_{i}.png")
 
 
-def visualise_roadgraph(roadgraph: Tensor, valid_mask: Tensor) -> None:
+def roadgraph(roadgraph: Tensor, valid_mask: Tensor) -> None:
     """Plot roadgraph segments from raw data"""
 
     for idx, (rg, mask) in enumerate(zip(roadgraph, valid_mask)):
@@ -145,9 +143,8 @@ def visualise_roadgraph(roadgraph: Tensor, valid_mask: Tensor) -> None:
         plt.close(figname)
 
 
-def visualise_roadmap(roadmap: Tensor) -> None:
+def roadmap(roadmap: Tensor) -> None:
     """Visualise the roadmap image"""
-
     for idx, im in enumerate(roadmap):
         figname = f"lanecenter_image_{idx}"
         plt.figure(figname, figsize=(20, 20))
@@ -168,7 +165,7 @@ def render_signals(signals: np.ndarray, im_shape) -> np.ndarray:
     return signal_image
 
 
-def overlay_roadmap_and_occupancy(
+def roadmap_and_occupancy(
     roadmaps: Tensor, occupancies: Tensor, signals: Tensor
 ) -> None:
     """Overlay both occupancy and roadmap image to ensure they're synchronised"""
@@ -179,15 +176,18 @@ def overlay_roadmap_and_occupancy(
     for bidx, (roadmap, occupancy_vec, signal) in enumerate(
         zip(roadmaps, occupancies, signals)
     ):
-        signal_map = render_signals(signal, roadmap.shape[-2:])
+        signal_map = render_signals(signal, occupancy_vec.shape[-2:])
+        roadmap = cv2.resize(
+            roadmap[0], occupancy_vec.shape[-2:], interpolation=cv2.INTER_LINEAR
+        )
         for tidx, occupancy in enumerate(occupancy_vec[0]):
             figname = f"occupancy_roadimg_{bidx}_{tidx}"
-            plt.figure(figname, figsize=(20, 20))
-            img = np.stack([roadmap[0], occupancy, signal_map], axis=-1)
+            plt.figure(figname, figsize=(10, 10))
+            img = np.stack([roadmap, occupancy, signal_map], axis=-1)
             if tidx == 1:  # SDC Target Frame
                 img = cv2.drawMarker(
                     img,
-                    (128, 192),
+                    (img.shape[0] // 2, img.shape[1] // 2 + 64),
                     (1, 1, 1),
                     cv2.MARKER_TILTED_CROSS,
                     markerSize=6,

train.py
@@ -1,12 +1,14 @@
+from argparse import Namespace as NS
+import logging
 from typing import Tuple, Dict, List
 
 from torch import Tensor, nn, no_grad
 from torch.profiler import record_function
 from konductor.trainer.pbar import pbar_wrapper
-from konductor.trainer.initialisation import get_training_parser, initialise_training
+from konductor.trainer.init import get_training_parser, init_training, cli_init_config
 from konductor.trainer.pytorch import (
     PyTorchTrainer,
-    PytorchTrainingModules,
+    PyTorchTrainingModules,
     PerfLogger,
     TrainingMangerConfig,
 )
@@ -77,28 +79,41 @@ class Trainer(PyTorchTrainer):
         to log loss during eval). If predictions are missing then accuracy logging will
         be skipped (if you don't want to log acc during training)
         """
-        for statistic in logger.logger_keys:
+        for statistic in logger.keys:
             if statistic == "loss" and losses is not None:
                 logger.log(statistic, {k: v.item() for k, v in losses.items()})
             elif preds is not None:
                 logger.log(statistic, preds, data[0])
 
 
+def setup(cli_args: NS) -> Trainer:
+    exp_config = cli_init_config(cli_args)
+    trainer_config = TrainingMangerConfig()
+    if cli_args.pbar:
+        trainer_config.pbar = pbar_wrapper
+
+    return init_training(
+        exp_config,
+        Trainer,
+        trainer_config,
+        {"occupancy": Occupancy},
+        PyTorchTrainingModules,
+    )
+
+
+def run(trainer: Trainer, epochs: int) -> None:
+    """Run training until epochs is reached"""
+    logging.info("Begin training")
+    while trainer.data_manager.epoch < epochs:
+        trainer.run_epoch()
+
+
 def main() -> None:
     cli_parser = get_training_parser()
     cli_parser.add_argument("--pbar", action="store_true")
     cli_args = cli_parser.parse_args()
-    trainer_config = TrainingMangerConfig()
-    if cli_args.pbar:
-        trainer_config.pbar = pbar_wrapper
-    trainer = initialise_training(
-        cli_args,
-        Trainer,
-        trainer_config,
-        {"occupancy": Occupancy},
-        PytorchTrainingModules,
-    )
-    trainer.run_epoch()
+    trainer = setup(cli_args)
+    run(trainer, cli_args.epochs)
 
 
 if __name__ == "__main__":