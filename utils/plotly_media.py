4eb3db5f3f90c374dfbfb4efe6ab90cd5f5e5799
Bryce Ferenczi <bryce.ferenczi@monash.edu>
2023-05-19 13:37:12 +1000

    auto-scale columns by not explictly adding rows, center images
    Signed-off-by: Bryce Ferenczi <bryce.ferenczi@monash.edu>

--- utils/plotly_media.py
+++ utils/plotly_media.py
@@ -32,31 +32,6 @@ layout = html.Div(
 )
 
 
-def get_thumbnail(path: Path) -> np.ndarray:
-    """Reads video and returns first frame"""
-    vid = cv2.VideoCapture(str(path))
-    _, im = vid.read()
-    vid.release()
-    return cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
-
-
-def add_thumbnail_to_grid(grid: List[List[dbc.Col]], video_path: Path, n_col: int = 3):
-    """Adds thumbnail to grid with a width of n_col (infinite rows)"""
-    if len(grid[-1]) == n_col:
-        grid.append([])
-
-    img = get_thumbnail(video_path)
-
-    grid[-1].append(
-        dbc.Col(
-            html.Img(
-                id={"type": "vid-thumbnail", "index": f"{video_path.stem}"},
-                src=Image.fromarray(img).resize((300, 300)),
-            ),
-        )
-    )
-
-
 @callback(
     Output("md-experiment", "options"),
     Input("root-dir", "data"),
@@ -65,6 +40,30 @@ def init_exp(root_dir: str):
     if len(EXPERIMENTS) == 0:
         fill_experiments(Path(root_dir), EXPERIMENTS)
     return [e.name for e in EXPERIMENTS]
+
+
+def get_thumbnail(path: Path) -> np.ndarray:
+    """Reads video and returns first frame"""
+    vid = cv2.VideoCapture(str(path))
+    _, im = vid.read()
+    vid.release()
+    return cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
+
+
+def add_thumbnail_to_grid(grid: List[dbc.Col], video_path: Path, n_col: int = 3):
+    """Adds thumbnail to grid with a width of n_col (infinite rows)"""
+    img = get_thumbnail(video_path)
+
+    grid.append(
+        dbc.Col(
+            html.Img(
+                id={"type": "vid-thumbnail", "index": f"{video_path.stem}"},
+                src=Image.fromarray(img).resize((300, 300)),
+                alt=video_path.stem,
+            ),
+            style={"text-align": "center"},
+        )
+    )
 
 
 @callback(
@@ -79,11 +78,11 @@ def update_thumbnails(root_dir: str, experiment: str, media: str):
         raise PreventUpdate
 
     exp = next(e for e in EXPERIMENTS if e.name == experiment)
-    children = [[]]
+    children = []
     for video_path in exp.root.glob(f"**/{media}/*.webm"):
         add_thumbnail_to_grid(children, video_path)
 
-    return [dbc.Row(c) for c in children]
+    return dbc.Row(children)
 
 
 @callback(
