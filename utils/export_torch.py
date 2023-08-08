04206f090cd850dc3a7473b59032a71b4aa31dbc
Bryce Ferenczi <bryce.ferenczi@monash.edu>
2023-08-08 13:19:34 +1000

    clear random piecewise for eval
    Signed-off-by: Bryce Ferenczi <bryce.ferenczi@monash.edu>

--- utils/export_torch.py
+++ utils/export_torch.py
@@ -74,6 +74,7 @@ def initialize(
     data_cfg.random_heatmap_count = 0
     data_cfg.heatmap_time = list(range(20, 91, 10))  # Standard eval timesteps
     data_cfg.heatmap_time = [t // data_cfg.time_stride for t in data_cfg.heatmap_time]
+    data_cfg.random_heatmap_piecewise.clear()  # Ensure piecewise random is cleared
 
     model: MotionPerceiver = get_model(exp_cfg).cuda()
     ckpt = torch.load(
