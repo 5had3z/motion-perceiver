82ff84b988ba23dff9b41f6865a68e509e55b908
Bryce Ferenczi <bryce.ferenczi@monash.edu>
2023-08-22 17:30:36 +1000

    fix output descriptor initialization
    Signed-off-by: Bryce Ferenczi <bryce.ferenczi@monash.edu>

--- src/dataset/plugins/scene_loader/src.hpp
+++ src/dataset/plugins/scene_loader/src.hpp
@@ -11,8 +11,8 @@ template <typename Backend>
 class LoadScene : public dali::Operator<Backend>
 {
 private:
-    uint64_t mImageChannels;
-    uint64_t mImageSize;
+    int64_t mImageChannels;
+    int64_t mImageSize;
     std::filesystem::path mMetadataFile;
     std::filesystem::path mImageFolder;
     std::unordered_map<std::string, double> mMapInfo;
@@ -22,8 +22,8 @@ private:
 public:
     inline explicit LoadScene(const dali::OpSpec& spec)
         : dali::Operator<Backend>(spec)
-        , mImageChannels{spec.GetArgument<uint64_t>("channels")}
-        , mImageSize{spec.GetArgument<uint64_t>("size")}
+        , mImageChannels{spec.GetArgument<int64_t>("channels")}
+        , mImageSize{spec.GetArgument<int64_t>("size")}
         , mMetadataFile{spec.GetArgument<std::string>("metadata")}
         , mImageFolder{spec.GetArgument<std::string>("src")}
     {
@@ -42,12 +42,14 @@ protected:
 
     bool SetupImpl(std::vector<dali::OutputDesc>& output_desc, const dali::Workspace& ws) override
     {
+        DALI_ENFORCE(ws.GetInputBatchSize(0) == ws.GetInputBatchSize(1) && "Input Batch size does not match");
+        DALI_ENFORCE(mImageChannels > 0 && mImageSize > 0 && "Image size and channels should be > 0");
         this->loadMetadata();
 
         output_desc.resize(1); // Only one output
         output_desc.back().type = dali::DALI_UINT8;
-        const dali::TensorShape<3> imageShape(mImageChannels, mImageSize, mImageSize);
-        output_desc.back().shape.make_uniform(ws.GetInputBatchSize(0), imageShape);
+        output_desc.back().shape = dali::TensorListShape<>::make_uniform(
+            ws.GetInputBatchSize(0), dali::TensorShape<3>{mImageChannels, mImageSize, mImageSize});
 
         return true;
     }
