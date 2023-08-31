5c5ee08c4f1ee6f780f4190e3c726dbf571b6c19
Bryce Ferenczi <bryce.ferenczi@monash.edu>
2023-08-31 13:15:12 +1000

    change waymo root, change messages to debug only
    Signed-off-by: Bryce Ferenczi <bryce.ferenczi@monash.edu>

--- src/dataset/__init__.py
+++ src/dataset/__init__.py
@@ -1,7 +1,8 @@
 from pathlib import Path
+import logging
 
 from nvidia.dali import plugin_manager
 
 for plugin_path in Path(__file__).parent.glob("**/*.so"):
     plugin_manager.load_library(str(plugin_path))
-    print(f"DALI: loaded plugin - {plugin_path.name}")
+    logging.debug("DALI: loaded plugin - %s", plugin_path.name)
