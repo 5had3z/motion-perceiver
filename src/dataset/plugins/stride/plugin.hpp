184ccb91a8797daf8450fb4581b86d6c397995dc
Bryce Ferenczi <bryce.ferenczi@monash.edu>
2023-09-05 15:26:30 +1000

    fix extent to include last element

--- src/dataset/plugins/stride/plugin.hpp
+++ src/dataset/plugins/stride/plugin.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <cmath>
 #include <dali/pipeline/operator/operator.h>
 
 namespace sliceop
@@ -36,18 +37,17 @@ protected:
     bool SetupImpl(std::vector<dali::OutputDesc>& outputDesc, const dali::Workspace& ws) override
     {
         bool isValid{true};
-
-        DALI_ENFORCE(mStride > 1, "Stride must be greater than 1");
         DALI_ENFORCE(ws.NumInput() == 1, "Only input of 1 supported");
 
         auto shapeList = ws.GetInputShape(0);
         isValid &= shapeList.ndim > mDimIdx;
-        DALI_ENFORCE(shapeList.ndim > mDimIdx, "Stride dimension exceeds tensor dimension");
+        DALI_ENFORCE_VALID_INDEX(mDimIdx, shapeList.ndim);
 
         for (int sIdx = 0; sIdx < shapeList.num_samples(); ++sIdx)
         {
             auto shape = shapeList.tensor_shape(sIdx);
-            shape[mDimIdx] /= mStride;
+            DALI_ENFORCE_IN_RANGE(mStride, 1, shape[mDimIdx]);
+            shape[mDimIdx] = std::ceil(shape[mDimIdx] / static_cast<double>(mStride));
             shapeList.set_tensor_shape(sIdx, shape);
         }
         outputDesc.resize(1, {shapeList, ws.GetInputDataType(0)});
